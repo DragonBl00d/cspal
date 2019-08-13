426261b2cdda8388a51457ce49982db725beacee
Mareena Kunjachan <mareenakunjachan@gmail.com>
2019-08-13 13:21:17 +0530

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,7 @@
-  var CACHE_NAME = 'cspal-v1';
-  var urlsToCache = [
-  '/index.html'
-  ];
-
 self.addEventListener('install', function(event) {
-    console.log('Installed service-worker.js', event);
+    console.log('Installed sw.js', event);
 });
 
 self.addEventListener('activate', function(event) {
-  console.log('Activated sw.js', event);
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return true;
-          // Return true if you want to remove this cache,
-          // but remember that caches are shared across
-          // the whole origin
-        }).map(function(cacheName) {
-          return caches.delete(CACHE_NAME);
-        })
-      );
-    })
-  );
+    console.log('Activated sw.js', event);
 });
